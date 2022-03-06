fd01cadfc6046202590cc6b404c86c14705a3c69
soyalumno <soy_alumno@yahoo.co.jp>
2022-03-06 20:41:50 +0900

    Init app

---
+++ eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "jquery": true,
+    "commonjs": true,
+    "es2021": true,
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": ["error", 2, {"SwitchCase": 1}],
+    // "quotes": ["error", "double"],
+    // "semi": ["error", "always"],
+    "no-unused-vars": ["warn", {"vars": "all", "args": "none"}],
+    "no-console": ["off"]
+  }
+};
